d748eb8f7bb55d4aeb174c8e6d3b35509cf614a9
riteshkhamitkar <89683381+riteshkhamitkar@users.noreply.github.com>
2022-07-21 14:45:53 +0530

    Add files via upload

---
+++ Reverse Linked List II (L-92).java
@@ -0,0 +1,37 @@
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(head== null)return null;
+        
+        ListNode cur = head, prev = null;
+            while(left>1)
+            {
+                prev=cur;
+                cur=cur.next;
+                left--;
+                right--;
+            }
+        
+        ListNode start = prev,tail=cur;
+        ListNode temp = null;
+        while(right>0)
+        {
+            temp = cur.next;
+            cur.next=prev;
+            prev=cur;
+            cur=temp;
+            right--;
+            
+            
+        }
+        if(start!=null){
+            start.next=prev;
+        }
+        else
+        {
+            head=prev;
+        }
+        tail.next=cur;
+        return head;
+        
+    }
+}
